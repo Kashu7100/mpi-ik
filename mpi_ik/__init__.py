97d7dac240d7347feb1e77b7229d968a5df04225
Kashu7100 <echo.six0566@gmail.com>
2026-02-10 23:12:14 -0500

    update version

--- mpi_ik/__init__.py
+++ mpi_ik/__init__.py
@@ -1,6 +1,6 @@
 """mpi_ik - Inverse kinematics solver for MANO, SMPL, and SMPL-H models."""
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 from mpi_ik.armatures import MANOArmature, SMPLArmature, SMPLHArmature
 from mpi_ik.config import CACHE_DIR, MANO_LEFT_MODEL_PATH, MANO_RIGHT_MODEL_PATH, SMPL_MODEL_PATH, SMPLH_MODEL_PATH
